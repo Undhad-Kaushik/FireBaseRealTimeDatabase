df2480efb49fa7c97be4459a6497970291aa708b
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-04-30 15:31:48 +0530

    Initial Commit

---
+++ FireBaseRealTimeDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseRealTimeDatabase
+//
+//  Created by undhad kaushik on 30/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
